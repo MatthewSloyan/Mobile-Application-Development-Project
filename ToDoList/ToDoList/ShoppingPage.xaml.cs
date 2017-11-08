356e94b9bcfd6b07762ca533e564c1b7f70106cb
MatthewSloyan <g00348036@gmit.ie>
2017-11-08 23:39:44 +0000

    Added design elements, icons and colours along with the Shopping List Page

--- ToDoList/ToDoList/ShoppingPage.xaml.cs
+++ ToDoList/ToDoList/ShoppingPage.xaml.cs
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ToDoList
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class ShoppingPage : Page
     {
-        public MainPage()
+        public ShoppingPage()
         {
             this.InitializeComponent();
         }
@@ -30,11 +30,6 @@ namespace ToDoList
         private void MenuButton_Click(object sender, RoutedEventArgs e)
         {
             SplitViewMenu.IsPaneOpen = !SplitViewMenu.IsPaneOpen;
-        }
-
-        private void ShoppingMenu_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(ShoppingPage));
         }
     }
 }
